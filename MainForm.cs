22be0c9e602d1a5ff59a518aad4b8281e113fab1
ricardointeractive <ricardointeractive@hotmail.com>
2013-12-10 23:07:18 -0600

    tarea 3 de programacion

---
+++ MainForm.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: RicardoAlfonso
+ * Date: 10/12/2013
+ * Time: 10:54 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tarea3_usuario
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+         if(textBox1.Text =="ricardo" && textBox2.Text =="1234")
+                        {
+                                MessageBox. Show("bienvenido al sistema");
+                                
+                        }
+                        else 
+                        
+                                MessageBox.Show("Acceso Denegado!!!");			
+		}
+	}
+}
